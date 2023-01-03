1c58aadadc37b4c3ef81cfce670f08f696e65c54
praneel1 <maitrapraneel@gmail.com>
2023-01-03 14:39:34 +0530

    added length

--- length.py
+++ length.py
@@ -1,3 +1,5 @@
 
-length = {"mm":0.0001, "cm":0.001,"dm":0.01, "m":1, "Dm":10, "hm":100,"km":1000, "ft":3.28084, "inch":39.3701}
+length = {"ym":1e-24,"zm":1e-21,"am":1e-18,"fm":1e-15,"pm":1e-12,"angstrom":1e-10,"nm":1e-9,"microm":1e-6,"mm":0.001, "cm":0.01,"dm":0.1, "m":1, "Dm":10, "hm":100,"km":1000}
  
+def convert_l (val, inputU, outputU):
+    return val*length[inputU]/length[outputU]
--- main.py
+++ main.py
@@ -12,17 +12,23 @@ while True:
 
 
 
-    if convert_type == "L":
-        val = float(input("Enter Value: "))
-        for unit in length.keys():
-            print(unit)
-        unit_in = input("unit: ")
-        if unit_in in length.keys:
+    if convert_type == "L":      #FOR LENGTH CONVERSIONS
+        available_units = length.keys()
+        val = float(input("Enter Value: "))   # Input Value
+        for unit in available_units:
+            print(unit)              
+        unit_in = input("unit: ")      #Input Unit
+        if unit_in in available_units:
             print("convert ", val , unit_in, "to? ")
-            for unit in length.keys():
-                print(unit, end=" ")
+            for unit in available_units:
+                print(unit, end=" ")    #Output Unit
             unit_out = input()
-            
+            if unit_out in available_units:
+                print("Converting",val,unit_in, "to", unit_out)
+                output = convert_l(val,unit_in,unit_out)
+                print(val,unit_in,"=",output,unit_out)
+            else:
+                continue
 
             
         else:
