9b8902a4f8154452999c2b063402a46e4169d2dd
praneel1 <maitrapraneel@gmail.com>
2023-01-11 11:22:40 +0530

    added currency

---
+++ currency.py
@@ -0,0 +1,25 @@
+currency = {"rs":1, "$":81.71, "gbp":99.41 ,"euro":87.84, "yen":0.62, "rub":1.17}
+
+def C():
+    available_units = currency.keys()
+    try:
+        val = float(input("Enter Value: "))   # Input Value
+        for unit in available_units:
+            print(unit,end="  ")              
+        unit_in = input("unit: ")      #Input Unit
+        if unit_in in available_units:
+            print("convert ", val , unit_in, "to? ")
+            for unit in available_units:
+                print(unit, end=" ")    #Output Unit
+            unit_out = input()
+            if unit_out in available_units:
+                print("Converting",val,unit_in, "to", unit_out)
+                output = val*currency[unit_in]/currency[unit_out]
+                print(val,unit_in,"=",round(output,2),unit_out)
+            else:
+                print("Unit not available")
+        else:
+            print("Unit not available")
+    
+    except ValueError:
+        print("Enter float numbers only")
--- main.py
+++ main.py
@@ -8,18 +8,21 @@
 from length import *
 from weight import *
 from volume import *
+from currency import *
 
 while True:
     convert_type = input("Enter type: L(Length), W(Weight), V(Volume), C(Currency), T(Temperature), S(Speed), A(Angles)")
 
 
 
-    if convert_type == "L":      #FOR LENGTH CONVERSIONS
+    if convert_type == "L":      
         L()
     elif convert_type == "W":
         W()
     elif convert_type == "V":
         V()
+    elif convert_type == "C":
+        C()
     else:
         continue
 
